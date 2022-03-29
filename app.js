9004738d760cd7164c0e95c9da269454233dc8d2
davidescobar17 <davidescobar17@outlook.com>
2022-03-28 18:41:38 -0700

    Add backend implementation to serve static files

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})  
+// serve static files if in production
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static(path.join(__dirname, '/build')));
+
+  app.get('/', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  })
+}
 
 app.listen(port, (err) => {
   if (err) return console.log(err);
